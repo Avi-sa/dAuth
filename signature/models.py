576208165c7ab8dc912805a7218b107d6fd159b6
Avi-sa <avivish000@gmail.com>
2021-02-23 09:00:17 +0530

    Third

--- signature/models.py
+++ signature/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import os
+def create_path(instance, filename):
+    return os.path.join(
+        instance.user.username,
+        filename
+    )
+
 class SignImg(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='images')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    img1 = models.ImageField(upload_to=create_path, blank=True)
+    img2 = models.ImageField(upload_to=create_path, blank=True)
+
 
     def __str__(self):
         return self.user.username
